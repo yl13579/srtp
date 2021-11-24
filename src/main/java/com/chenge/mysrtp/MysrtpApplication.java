19b8380ced9ed267a194c689049a9b423e3c3532
30838 <3083842408@qq.com>
2021-11-24 17:43:28 +0800

    基础修改

---
+++ src/main/java/com/chenge/mysrtp/MysrtpApplication.java
@@ -0,0 +1,13 @@
+package com.chenge.mysrtp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysrtpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysrtpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chenge/mysrtp/MysrtpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenge.mysrtp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysrtpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
